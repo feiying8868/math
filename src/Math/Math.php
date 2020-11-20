b7f369b34f7529c99b53b673cdac2113746308ff
qiaohuan <shandongqiaohuan@163.com>
2020-11-20 11:54:15 +0800

    修改提交

--- src/Math/Math.php
+++ src/Math/Math.php
@@ -5,4 +5,10 @@ class Math{
     public function sum($a,$b){
         return $a+$b;
     }
+    public function cheng($a,$b){
+        return $a*$b;
+    }
+    public function chu($a,$b){
+        return $a/$b;
+    }
 }
